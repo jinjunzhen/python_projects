98998f51806ebc4d303c0b53e70d845b8b50e29e
jinjun zhen <jinjunzhen@yahoo.com>
2021-04-02 22:17:53 -0700

    day_25

--- day_24_local_file/main.py
+++ day_24_local_file/main.py
@@ -1,16 +1,3 @@
-# This is a sample Python script.
+with open("my_file.txt", 'w') as file:
+    file.write("bye jjz")
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
