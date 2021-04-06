1f3d03071f9c2ef7afbf20b41028739d86cf779c
jinjun zhen <jinjunzhen@yahoo.com>
2021-04-06 00:17:37 -0700

    send_email_with_python

---
+++ send_email_with_python/main.py
@@ -0,0 +1,13 @@
+import smtplib
+
+my_email = 'jinjunzhen.testing'
+password = 'jz@G815049'
+
+with smtplib.SMTP('smtp.gmail.com', 587) as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=password)
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs='jinjunzhen@yahoo.com',
+        msg='Subject:testing python email\n\nThis is the body of my email again'
+    )
