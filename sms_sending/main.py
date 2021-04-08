4c1512e4a25d1952efc2045a778956fa26bc758e
jinjun zhen <jinjunzhen@yahoo.com>
2021-04-08 01:16:51 -0700

    sms able to send sms to phone from python + environmental variable

--- sms_sending/main.py
+++ sms_sending/main.py
@@ -1,9 +1,11 @@
 import os
 from twilio.rest import Client
+from dotenv import load_dotenv
 
 ## register on "https://www.twilio.com/" and get [account_sid] [auth_token] and [PHONE NUMBER]
-account_sid = "account_sid"
-auth_token = "auth_token"
+load_dotenv()
+account_sid = os.environ.get("account_sid")
+auth_token = os.environ.get("auth_token")
 
 
 client = Client(account_sid, auth_token)
