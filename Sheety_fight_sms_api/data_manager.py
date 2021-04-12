302887448d54855568d82dcf3c643e9ac8efa6b9
jinjun zhen <jinjunzhen@yahoo.com>
2021-04-12 13:45:28 -0700

    1:45pm

--- Sheety_fight_sms_api/data_manager.py
+++ Sheety_fight_sms_api/data_manager.py
@@ -1,3 +1,33 @@
+import requests
+import os
+
+SHEETY_URL = os.environ["sheety_url"]
+HEADERS = {
+    "Authorization" : os.environ["Authorization"]
+}
+
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
-    pass
+    def __init__(self):
+        self.data_object = {}
+
+    def get_data_google_doc(self):
+        response = requests.get(SHEETY_URL, headers=HEADERS)
+        data = response.json()
+        return data
+
+    def update_iataCode_google_doc(self):
+        print(self.data_object["sheet1"])
+        for row in self.data_object["sheet1"]:
+            new_data = {
+                "sheet1" : {
+                    "iataCode" : row["iataCode"]
+                }
+            }
+
+            response = requests.put(
+                url=f"{SHEETY_URL}/{row['id']}",
+                json=new_data,
+                headers=HEADERS
+            )
+            print(response.text)
--- Sheety_fight_sms_api/main.py
+++ Sheety_fight_sms_api/main.py
@@ -1,1 +1,16 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+from data_manager import DataManager
+from flight_search import FlightSearch
+
+
+data_case = DataManager()
+flight_search = FlightSearch()
+my_data = data_case.get_data_google_doc()
+
+
+for row in my_data["sheet1"]:
+    if row["iataCode"] == "":
+        row["iataCode"] = "fill in here"
+
+data_case.data_object = my_data
+data_case.update_iataCode_google_doc()
