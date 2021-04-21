19c222d0b6f5b7d8a6e4b5112f0f24c794cb4781
jinjun zhen <jinjunzhen@yahoo.com>
2021-04-21 09:54:34 -0700

    3

--- draft_3/main.py
+++ draft_3/main.py
@@ -1,16 +1,36 @@
-# This is a sample Python script.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+import requests
+import lxml
+from bs4 import BeautifulSoup
+import smtplib
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+ZILLOW_ONE = 'https://docs.google.com/forms/d/e/1FAIpQLSe6lAosmVKRF8Cv-CfP2' \
+             'FvNDnwyVwfJw3hvbUYpygxYjhfJWg/viewform?usp=sf_link'
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+url = "https://www.amazon.com/Monster-Hunter-Rise-Nintendo-Switch/dp/B08JJ37XVW/" \
+      "ref=sr_1_1_sspa?crid=51GIU65E8X8Z&dchild=1&keywords=monster+hunter+rise&qid=" \
+      "1618679740&sprefix=monster+hunter+%2Caps%2C241&sr=8-1-spons&psc=1&spLa=" \
+      "ZW5jcnlwdGVkUXVhbGlmaWVyPUEyNUVGMFY2QTdIUkYzJmVuY3J5cHRlZElkPUEwNDc1MTUzMVc4S1l" \
+      "BTlVaU1dORSZlbmNyeXB0ZWRBZElkPUEwMzEwNzU0M0dWQ1NTTEszQjFWOSZ3aWRnZXROYW1lPXNwX2F0Z" \
+      "iZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU="
+header = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_0_1) AppleWebKit/537.36 "
+                  "(KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36",
+    "Accept-Language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7,ja;q=0.6,zh-TW;q=0.5"
+}
+
+response = requests.get(url, headers=header)
+
+soup = BeautifulSoup(response.content, "lxml")
+print(soup.prettify())
+with open("output1.html", "w") as file1:
+    file1.write(str(soup))
+
+price = soup.find(id="priceblock_ourprice").get_text()
+price_without_currency = price.split("$")[1]
+price_as_float = float(price_without_currency)
+print(price_as_float)
+
+title = soup.find(id="productTitle").get_text().strip()
+print(title)
