3544bb8b9e98c3302d9ca812cf8dd42c8b801c31
jinjun zhen <jinjunzhen@yahoo.com>
2021-04-22 11:17:16 -0700

    draft

--- draft/app.py
+++ draft/app.py
@@ -10,7 +10,17 @@ from flask import Flask, escape, request
 app = Flask(__name__)
 
 
+def make_bold(function):
+    def the_bold():
+        return f"<b>{function()}</b>"
+    return the_bold
 
+
+
+@app.route('/bye')
+@make_bold
+def say_bye():
+    return "Bye!"
 
 
 @app.route('/')
@@ -18,5 +28,9 @@ def hello():
     name = request.args.get("name", "World")
     return f'Hello, {escape(name)}!'
 
+@app.route('/<name>')
+def greeting(name):
+    return f"What's up {name}"
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
